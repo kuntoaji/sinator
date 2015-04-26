4de55533555a5935089500766f63f3118881de4e
kuntoaji <kunto.aji.kr@gmail.com>
2015-04-26 18:29:57 +0700

    move melodiest setting spec to the proper place :)

--- spec/melodiest/setting_spec.rb
+++ spec/melodiest/setting_spec.rb
@@ -1,5 +1,19 @@
 describe Melodiest::Setting do
-  it "respond to setup" do
-    expect(Melodiest::Setting.method_defined?(:setup)).to be_truthy
+  describe ".setup" do
+    let(:my_app) do
+      class MyApp < Melodiest::Application
+        setup server: 'puma'
+      end
+
+      MyApp
+    end
+
+    it "set thin as server" do
+      expect(app.settings.server).to eq("thin")
+    end
+
+    it "overrides default setting" do
+      expect(my_app.settings.server).to eq("puma")
+    end
   end
 end
--- spec/melodiest_spec.rb
+++ spec/melodiest_spec.rb
@@ -1,24 +1,4 @@
 describe Melodiest::Application do
-  context ".settings" do
-    it "use thin as server" do
-      expect(app.settings.server).to eq("thin")
-    end
-
-    describe ".setup" do
-      let(:my_app) do
-        class MyApp < Melodiest::Application
-          setup server: 'puma'
-        end
-
-        MyApp
-      end
-
-      it "overrides default setting" do
-        expect(my_app.settings.server).to eq("puma")
-      end
-    end
-  end
-
   context ".extensions" do
     it "has reloader extension" do
       expect(app.extensions.include?(Sinatra::Reloader)).to be_truthy
